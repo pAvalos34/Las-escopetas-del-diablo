099324c5f1dd3c71a1d9e96cb52780867296257f
thejopah <thejopah@gmail.com>
2015-07-11 10:15:46 -0500

    agregado main

---
+++ Main.py
@@ -0,0 +1,11 @@
+"""
+Dust in the barrel, by Lonely Pug
+Code by Pablo Avalos
+2015-07-11
+
+Juego de exploración con godview, pistolas y animalitos
+"""
+import pygame, sys
+from pygame.locals import *
+from random import randint
+
