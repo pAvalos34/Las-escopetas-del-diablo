cf5f1435ab986c80a800c7b315f071b92b214865
thejopah <thejopah@gmail.com>
2015-07-11 12:56:00 -0500

    IT´S ALIVEEEEE HAHAH

--- Main.py
+++ Main.py
@@ -5,12 +5,41 @@ Code by Pablo Avalos
 
 Juego de exploracion con godview, pistolas y animalitos 
 """
-
 import pygame, sys
 from pygame.locals import *
 
-def Main():
-	pass
+#Paleta de colores
+class color:
+	Pantone_C = (0, 146, 188)
 
+pygame.init()
+screen = pygame.display.set_mode((800,600))
+pygame.display.set_caption("Dust in the barrel")
+Walbert = pygame.image.load("Img/Walbert.png")
+
+
+def Main():
+	PosX = 50
+	PosY = 100
+	Vel = 20
+	
+	while True:
+		screen.fill(color.Pantone_C)
+		screen.blit(Walbert, (PosX, PosY))
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				pygame.quit()
+				sys.exit()
+			elif event.type == pygame.KEYDOWN:
+				if event.key == K_LEFT:
+					PosX =  PosX - Vel
+				elif event.key == K_RIGHT:
+					PosX = PosX + Vel
+				elif event.key == K_UP:
+					PosY = PosY - Vel
+				elif event.key == K_DOWN:
+					PosY = PosY + Vel
+								
+		pygame.display.update()
 
 Main()
