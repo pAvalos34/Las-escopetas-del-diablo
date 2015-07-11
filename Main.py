c6cf175fea4e769181ea7c3de79a496068bb2e7d
thejopah <thejopah@gmail.com>
2015-07-11 10:45:27 -0500

    cero

--- Main.py
+++ Main.py
@@ -1,24 +1,16 @@
-""" Dust in the barrel, by Lonely Pug
-	Code by Pablo Avalos
-	2015-07-11
+""" 
+Dust in the barrel, by Lonely Pug
+Code by Pablo Avalos
+2015-07-11
 
-	Juego de exploracion con godview, pistolas y animalitos """
+Juego de exploracion con godview, pistolas y animalitos 
+"""
 
 import pygame, sys
 from pygame.locals import *
 
 def Main():
-	print "Hola"
-	print "Tengo un numero entre 1 y 100"
-	Respuesta = False #Flag para identificar 
-	Num = 34 #Numero de la computadora
+	pass
 
-	while not Respuesta:
-		NumUsu = input ("Dime cual es:	")
-		if NumUsu == Num:
-			print "Ese era"
-			Respuesta = True
-		else:
-			print "Tenemos toda el tiempo del mundo, soy de 64 bits"
 
 Main()
