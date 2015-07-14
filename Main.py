1fc73628f733d0e4d5071145f74b85ec99619407
thejopah <thejopah@gmail.com>
2015-07-14 10:21:13 -0500

    Add comments

--- Main.py
+++ Main.py
@@ -1,9 +1,9 @@
-""" 
+"""
 Dust in the barrel, by Lonely Pug
 Code by Pablo Avalos
 2015-07-11
 
-Juego de exploracion con godview, pistolas y animalitos 
+Juego de exploracion con godview, pistolas y animalitos
 """
 import pygame, sys
 from pygame.locals import *
@@ -14,22 +14,23 @@ class color:
 
 pygame.init()
 screen = pygame.display.set_mode((800,600))
-pygame.display.set_caption("Dust in the barrel")
+pygame.display.set_caption("Dust in the barrel") #Titulo de la ventana
 Walbert = pygame.image.load("Img/Walbert.png")
 
 
 def Main():
-	PosX = 50
-	PosY = 100
-	Vel = 20
-	
-	while True:
-		screen.fill(color.Pantone_C)
-		screen.blit(Walbert, (PosX, PosY))
+	PosX, PosY = 50, 100 #Posicion inicial
+	Vel = 20 #Velocidad de Movimiento
+
+	while True: #Loop para iniciar el juego
+		screen.fill(color.Pantone_C) #Fondo
+		screen.blit(Walbert, (PosX, PosY)) #Posicion del personaje
+
 		for event in pygame.event.get():
 			if event.type == QUIT:
 				pygame.quit()
 				sys.exit()
+			#Movimiento con flechas de teclado
 			elif event.type == pygame.KEYDOWN:
 				if event.key == K_LEFT:
 					PosX =  PosX - Vel
@@ -39,7 +40,7 @@ def Main():
 					PosY = PosY - Vel
 				elif event.key == K_DOWN:
 					PosY = PosY + Vel
-								
+
 		pygame.display.update()
 
 Main()
